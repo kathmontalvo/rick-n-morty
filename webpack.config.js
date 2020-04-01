9b9172d23e49902611a67e2c88c3f3e8c6ca2d46
kmontalvom <kmontalvom@ucic.pe>
2020-04-01 01:30:31 -0500

    setup webpack config

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html?$/,
+        use: {
+          loader: "html-loader",
+          options: { minimize: true }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack 4 Starter",
+      template: "public/index.html",
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false
+      },
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
